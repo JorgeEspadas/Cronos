4ce348f12d57bf8d1c501e387746fcd2509ae1a7
ezpadaz <al050610@uacam.mx>
2019-10-19 11:20:50 -0500

    Progreso actual.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html lang = "es">
+    <head>
+        <title>Registro</title>
+        <link rel = "stylesheet" href = "css/style-register.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <meta charset = "utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shortcut icon" href="image/logo-apolo.jpg">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <img id = "fdi" src = "image/fi.png" alt = "FDI" >
+                <img id = "uac" src = "image/uac-logo.png" alt = "UAC">
+            </div>
+        </header>
+
+        <?php
+            echo 'sup';
+        ?>
+
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
