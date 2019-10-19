d71d5b6af8d9b2d3d84c2c67ce743334e0284781
EzpadaZ <EzpadaZ@DESKTOP-4EPG6L1>
2019-10-19 15:08:02 -0500

    Agregado el contenido de angel, errores presentes en el diseno.

--- index.php
+++ index.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
 <html lang = "es">
     <head>
-        <title>Registro</title>
-        <link rel = "stylesheet" href = "css/style-register.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <meta charset = "utf-8">
+        <title>Cronos</title>
+        <link rel = "stylesheet" href = "./css/style-register.css">
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset = "utf-8">
         <link rel="shortcut icon" href="image/logo-apolo.jpg">
     </head>
     <body>
         <header>
-            <div class="container">
-                <img id = "fdi" src = "image/fi.png" alt = "FDI" >
-                <img id = "uac" src = "image/uac-logo.png" alt = "UAC">
+            <div class = "container-fluid">
+                <img id = "uac" src = "image/uac-logo.png" alt = "UAC" width="95" height="95">
+                <img id = "fdi" src = "image/fi.png" alt = "FDI" width="95" height="95">
             </div>
         </header>
-
-        <?php
-            echo 'sup';
-        ?>
-
-        <script src="js/jquery-3.4.1.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+        
+        <main class = "contairner">
+            <div class="col-8  col-sm-6 col-lg-4 offset-2 offset-sm-3 offset-lg-4">
+                <div class="card-body">
+                        <h3 class="card-title text-center">Inicio</h3>
+                 </div>
+                <img class="card-img-top" src="image/lector-prueba.gif" alt="Card image cap" width="30%" height="40%">
+                <div class="card-body text-center">
+                        <h4 class="card-text">Escanea tu credencial porfavor.</h4>
+                 </div>
+            </div>
+        </main>
+        
+        
+        <script src="./js/jquery-3.4.1.min.js"></script>
+        <script src="./js/popper.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
     </body>
 </html>
