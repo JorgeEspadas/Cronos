cd7cf3be5663305000658be557ee4cfeefef5571
EzpadaZ <EzpadaZ@DESKTOP-4EPG6L1>
2019-10-19 15:11:44 -0500

    Agregado folder config, modificado texto de housekeeper.

--- housekeeper/index.php
+++ housekeeper/index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE  html>
 <html lang = "es">
     <head>
-        <title>Login</title>
-
+        <title>Administracion</title>
         <link rel = "stylesheet" href = "../css/estilos.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,20 +9,15 @@
         <link rel="shortcut icon" href="../image/logo-apolo.jpg">
     </head>
     <body class="text-center">
+        <!-- Alguien porfavor ponga acentos y la enie ptm. -->
         <form class="col-10 col-sm-5 col-lg-4 col-xl-3 form-signin">
             <img class="mb-4" src="../image/logo-apolo.jpg" alt="" width="140" height="100">
-            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <label for="inputEmail" class="sr-only">Email address</label>
+            <h1 class="h3 mb-3 font-weight-normal">Portal de Acceso</h1>
+            <label for="inputEmail" class="sr-only">Matricula de Acceso</label>
             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
+            <label for="inputPassword" class="sr-only">Contrasena</label>
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-            <!--<div class="checkbox mb-3">
-                <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-            </div>-->
-            <button class="btn btn-light btn-lg btn-block" type="submit">Sign in</button>
-            <!--<p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>-->
+            <button class="btn btn-light btn-lg btn-block" type="submit">Ingresar</button>
         </form>
 
         <script src="../js/jquery-3.4.1.min.js"></script>
@@ -31,3 +25,5 @@
         <script src="../js/bootstrap.min.js"></script>
     </body>
 </html>
+
+
