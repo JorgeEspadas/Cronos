ed9daa7acd39e6a7c8a658c58ff5ed09ed6d9e46
2019UserVillanueva <56413675+2019UserVillanueva@users.noreply.github.com>
2019-10-23 19:22:38 -0500

    Update guardar_alumno.php

--- modulos/guardar_alumno.php
+++ modulos/guardar_alumno.php
@@ -53,11 +53,12 @@ if(isset($_POST['matricula']) &&isset($_POST['nombre']) && isset($_POST['apellid
  <html lang="es">
 
     <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="css/bootstrap.min.css" rel="stylesheet">
-      <link href="css/bootstrap-theme.css" rel="stylesheet">
-      <script src="js/jquery-3.1.1.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
+        <meta charset = "utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/bootstrap-theme.css" rel="stylesheet">
+        <script src="../js/jquery-3.4.1.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
 
     </head>
 
