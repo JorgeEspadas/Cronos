659743888655815eb3ae04d6c8974ed988cc057d
Jorge E. Espadas <45724402+AnotherPlayer@users.noreply.github.com>
2019-10-24 16:00:42 -0500

    Quite espacios en blanco.

--- modulos/conexion.php
+++ modulos/conexion.php
@@ -1,19 +1,12 @@
 <?php
 class Conexion{
-    /*
-     * Retorna el objeto de conexion :v no se si este bien porfavor revisalo.
-     * Se que se usa el "require once" en estas cosas de base de datos.
-     * No se si esta bien manejarlo como un objeto tal cual a lo java.       
-     */
    function getConnection(){
         require_once('reader.php');
         $config = reader::readJSON('../config/mysql_config.json');
-        
-     $conn = mysqli_connect($config["IP"], $config["USR"], $config["PWD"], $config["DB"]);
-       mysqli_set_charset($conn,"UTF8");
-       
+        //no se por que sacaste esto del return :v
+        $conn = mysqli_connect($config["IP"], $config["USR"], $config["PWD"], $config["DB"]);
+        mysqli_set_charset($conn,"UTF8");       
       return $conn;
-       
     }   
 }
 ?>
