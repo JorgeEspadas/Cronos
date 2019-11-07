43573b67453215636a9f5766c45ca746d1fa5d4c
Angel <al045219@uacam.mx>
2019-11-07 10:11:13 -0600

    Se incluyeron nuevos logos, se modifico el diseño de acceso.php.

--- housekeeper/acceso.php
+++ housekeeper/acceso.php
@@ -5,20 +5,21 @@
         <link rel = "stylesheet" href = "../css/estilos.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../fonts/ionicons.min.css">
         <meta charset = "utf-8">
         <link rel="shortcut icon" href="../image/logo-apolo.jpg">
     </head>
-    <body class="text-center">
-        <!-- Alguien porfavor ponga acentos y la enie ptm. -->
-        <form class="col-10 col-sm-5 col-lg-4 col-xl-3 form-signin" method="post" action="login.php">
-            <img class="mb-4" src="../image/logo-apolo.jpg" alt="" width="140" height="100">
-            <h1 class="h3 mb-3 font-weight-normal">Portal de Acceso</h1>
-            <label for="inputEmail" class="sr-only">ID de Empleado</label>
-            <input type="text" name="id_empleado" id="id_empleado" class="form-control" placeholder="ID. De Empleado" required autofocus>
-            <label for="inputPassword" class="sr-only">Contraseña</label>
-            <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-            <button class="btn btn-light btn-lg btn-block" type="submit">Ingresar</button>
-        </form>
+    <body>
+        <div class="login-dark">
+        <form method="post" action="login.php">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+            <div class="form-group"><input class="form-control" type="text" name="id_empleado" id="id_empleado" placeholder="ID. De Empleado" required autofocus></div>
+            <div class="form-group"><input class="form-control" type="password" name="password" id="password" placeholder="Password" required></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Ingresar</button></div>
+            <!--<a class="forgot" href="#">Forgot your email or password?</a>-->
+            </form>
+        </div>
         <script src="../js/jquery-3.4.1.min.js"></script>
         <script src="../js/popper.min.js"></script>
         <script src="../js/bootstrap.min.js"></script>
--- housekeeper/login.php
+++ housekeeper/login.php
@@ -1,9 +1,10 @@
 <html>
     <head>
         <title>Inicio de Sesion</title>
-        <link rel = "stylesheet" href = "../css/style-register.css">
+        <link rel = "stylesheet" href = "../css/estilos.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../fonts/ionicons.min.css">
         <meta charset = "utf-8">
         <link rel="shortcut icon" href="../image/logo-apolo.jpg">
     </head>
@@ -20,41 +21,39 @@
                     $result = $stmt->get_result();
                     $user = $result->fetch_object();                  
                     if($user == null){
-                        echo '<main class = "container d-flex justify-content-center align-items-center" id="accordion-main" >
-                                    <div class="row card  d-block" style="width: 18rem;">
-                                        <div class="card-body  text-center">
-                                            <h4 class="card-text">El usuario introducido no existe.</h4>
-                                        </div>
-                                    </div>
-                                </main>';
-                        header("Refresh:3; url=../housekeeper/acceso.php");
+                        echo '<div class="col-3  alert alert-warning text-center pt-4" id="alert" style="transform: translate(150%, 0%);">
+                                <p>El usuario introducido no existe.</p>
+                            </div>';
+                      header("Refresh:2; url=../housekeeper/acceso.php");
                     }else{
                         //SEGURIDAD NULA. Lo corregire apenas haga el modulo de hashing.
                         if ($_POST['password'] == $user->password) {
                             $_SESSION['session_id'] = $user->id_empleado;
-                            echo '<main class = "container d-flex justify-content-center align-items-center" id="accordion-main" >
-                                    <div class="row card  d-block" style="width: 18rem;">
-                                        <div class="card-body  text-center">
-                                            <h4 class="card-text">Bienvenido al Sistema.</h4>
-                                        </div>
-                                    </div>
-                                </main>';
-                            header("Refresh:3; url=../housekeeper/home.php");
+                            header("Refresh:2; url=../housekeeper/home.php");
                         } else {
                         //Login incorrecto.
-                            echo '<main class = "container d-flex justify-content-center align-items-center" id="accordion-main" >
-                                    <div class="row card  d-block" style="width: 18rem;">
-                                        <div class="card-body  text-center">
-                                            <h4 class="card-text">Password Incorrecto</h4>
-                                        </div>
-                                    </div>
-                                </main>';
-                            header("Refresh:3; url=../housekeeper/acceso.php");
+                            echo '<div class="col-3  alert alert-warning text-center pt-4" id="alert" style="transform: translate(150%, 0%);">
+                                <p>Password Incorrecta.</p>
+                            </div>';
+                            header("Refresh:2; url=../housekeeper/acceso.php");
                         }
                     $con -> close();
                     }
                 }   
             }
         ?>
+    <div class="login-dark">
+        <form method="post" action="login.php" style="padding-top:60px;  padding-bottom: 40px;  transform: translate(-50%, -40%);">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration" style="margin-bottom: 30px;"><i class="icon ion-ios-locked-outline"></i></div>
+            <div class="form-group"><input class="form-control" type="text" name="id_empleado" id="id_empleado" placeholder="ID. De Empleado" required autofocus></div>
+            <div class="form-group"><input class="form-control" type="password" name="password" id="password" placeholder="Password" required></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Ingresar</button></div>
+            <!--<a class="forgot" href="#">Forgot your email or password?</a>-->
+            </form>
+        </div>
+        <script src="../js/jquery-3.4.1.min.js"></script>
+        <script src="../js/popper.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
     </body>
 </html>
